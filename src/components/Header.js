aa05c2d851c5ef580a15424e071a6b100e14c432
Paulo Eduardo Campos Afonso <pauloeduardocamposafonso@Akhenaton.local>
2018-11-25 21:52:09 -0200

    First Version

---
+++ src/components/Header.js
@@ -0,0 +1,159 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { connect } from "react-redux";
+import { media, color } from 'styles/utils';
+import { NavLink, Link } from 'react-router-dom';
+
+import SiteTitle from './SiteTitle';
+
+const Wrapper = styled.header`
+  flex: 0 0 auto;
+  padding: .5rem 1rem;
+  background: #f3f3f3;
+  z-index: 999999;
+  .logo-infoamazonia {
+    margin-bottom: 1rem;
+    vertical-align: middle;
+    ${media.phablet`
+      margin-bottom: 0.4rem!important;
+    `}
+    ${media.desktop`
+    margin-bottom: 0.6rem!important;
+  `}
+  }
+  .logo-am {
+    vertical-align: middle;
+  }
+  .header-content {
+    display: flex;
+    flex-direction: row;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  ${media.phablet`
+    padding: 0.4rem 2rem 0.4rem 0.4rem;
+    font-size: 1em;
+  `}
+  ${media.desktop`
+    padding: 1.5vw 8vw;
+  `}
+  ${media.desktopHD`
+    padding: 1.5vw 10vw;
+  `}
+  .site-title {
+    flex: 1 1 100%;
+    font-size: .6em;
+  }
+  img {
+    width: auto;
+    height: auto;
+    max-height: 2vh;
+    margin: 0 1rem;
+    ${media.phablet`
+    max-height: 15vh;
+    max-width: 18vw;
+    `}
+    ${media.phone`
+      max-height: 15vh;
+      max-width: 18vw;
+    `}
+    ${media.desktop`
+      margin: 0 2rem;
+      max-height: 5vh;
+    `}
+    ${media.desktopHD`
+      margin: 0 2rem;
+      max-height: 5vh;
+    `};
+  }
+  nav {
+    flex: 0 0 auto;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    font-size: .8em;
+    a {
+      position: relative;
+      color: #999;
+      margin-left: 1.5rem;
+      transition: all .2s ease-in-out;
+      ${media.desktop`
+        margin-left: 1rem;
+        background: #fff;
+        width: 2em;
+        height: 2em;
+        padding: .3em;
+        border-radius: 100%;
+        line-height: 2em;
+        text-align: center;
+        font-size: 1em;
+      `}
+      &.active,
+      &:hover,
+      &:active,
+      &:focus {
+        color: #111;
+      }
+      &.active {
+        transform: scale(1.3);
+      }
+      .icon-info {
+        background: red;
+        color: white;
+        position: absolute;
+        width: 1rem;
+        height: 1rem;
+        line-height: 1rem;
+        text-align: center;
+        top: -.5rem;
+        right: -.5rem;
+        display: inline-block;
+        text-decoration: none;
+        font-size: .5em;
+        border-radius: 100%;
+        font-weight: 800;
+      }
+    }
+  }
+`
+
+class Header extends Component {
+  render () {
+    const { lastPath,aboutPath, sharePath } = this.props;
+    return (
+      <Wrapper>
+        <div className="header-content">
+            <div className="site-title clearfix">
+                <a href="https://infoamazonia.org/" target="_blank">
+                  <img src={require("images/partners/infoamazonia-p.png")} className="logo-infoamazonia" />
+                </a>
+            </div>
+          <nav>
+          {
+            aboutPath &&
+            <NavLink to={aboutPath} title="Créditos">
+              <span className="fa fa-info"></span>
+            </NavLink>
+          }
+          {
+            sharePath &&
+            <NavLink to={sharePath} title="Compartilhe">
+              <span className="fa fa-share-alt"></span>
+            </NavLink>
+           }
+          </nav>
+        </div>
+      </Wrapper>
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    lastPath: state.context.lastPath,
+    aboutPath: state.context.aboutPath,
+    sharePath: state.context.sharePath
+  };
+};
+
+export default connect(mapStateToProps, null)(Header);
---
+++ src/components/Mapbox.js
@@ -0,0 +1,58 @@
+/* global window */
+import React, {Component} from 'react';
+import ReactMapGL from 'react-map-gl';
+//import MAP_STYLE from './style.json';
+// import MAP_STYLE from './mapbox/styles/map1.json';
+import {fromJS} from 'immutable';
+import PropTypes from 'prop-types';
+import Dimensions from 'react-dimensions';
+import LegendMap1 from './mapbox/LegendMap1';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiaW5mb2FtYXpvbmlhIiwiYSI6InItajRmMGsifQ.JnRnLDiUXSEpgn7bPDzp7g'; // Set your mapbox token here
+
+class MapBox extends Component {
+
+  state = {
+    mapStyle: '',
+    viewport: {
+      width: this.props.containerWidth,
+      height: this.props.containerHeight,
+      latitude: 37.7577,
+      longitude: -122.4376,      
+      zoom: 8
+    }
+  };
+
+  _onViewportChange = viewport => this.setState({viewport});
+  _onStyleChange = mapStyle => this.setState({mapStyle});
+
+  static propTypes = {
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    // console.log(this.props.containerWidth)
+    // this.setState({viewport:{
+    //   width: this.props.containerWidth,
+    //   height: this.props.containerHeight,
+    // }});
+  }
+
+  render() {
+    const {viewport, mapStyle} = this.state;
+    return (
+      <ReactMapGL
+        {...viewport}
+        mapStyle={mapStyle}
+        onViewportChange={(viewport) => this.setState({viewport})}
+        //mapStyle={defaultMapStyle}
+        mapboxApiAccessToken={MAPBOX_TOKEN} >
+        <LegendMap1 containerComponent={this.props.containerComponent} onChange={this._onStyleChange} />
+      </ReactMapGL>
+    );
+  }
+}
+
+export default Dimensions({elementResize: true})(MapBox)
